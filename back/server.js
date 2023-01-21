114e6c070e449fdc1a46980096418716b9422832
kdtTetz <xenosign@naver.com>
2023-01-21 11:25:18 +0900

    Update server.js

--- back/server.js
+++ back/server.js
@@ -17,11 +17,6 @@ app.get('/', (req, res) => {
       age: '4',
       nickName: '공주님',
     },
-    {
-      name: '크롱이',
-      age: '5',
-      nickName: '장난꾸러기',
-    },
   ];
 
   res.status(200).send(JSON.stringify(pororoObjArr));
